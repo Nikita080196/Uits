bba29dde14301c58dda21f87ce2eda2e0113ead2
Nikita080196 <vocabulary080196@gmail.com>
2017-05-17 15:56:57 +0300

    ad lesson6.task1

--- src/lesson6/task1/Main.java
+++ src/lesson6/task1/Main.java
@@ -1,7 +1,15 @@
 package lesson6.task1;
 
-/**
- * Created by Валентина on 16.05.2017.
- */
 public class Main {
+    public static void main(String[] args) {
+        int k = 5;
+        int c;
+        int i =1;
+
+        do{
+            c = i% k;
+            if( c == 0)
+                System.out.println(i);
+        }while (i++<=100);
+    }
 }
