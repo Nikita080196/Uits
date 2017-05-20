3203ed1452b17d7da190e5cb0a81cf80c57c88dc
Nikita080196 <vocabulary080196@gmail.com>
2017-05-20 14:44:42 +0300

    add bubble sort lesson6.task2

--- src/lesson6/task2/Sort.java
+++ src/lesson6/task2/Sort.java
@@ -1,7 +1,32 @@
 package lesson6.task2;
 
-/**
- * Created by Валентина on 16.05.2017.
- */
+import java.util.Arrays;
+
 public class Sort {
+    public static void main(String[] args) {
+        int[][] mas = {
+                {12, 456, 64225,},
+                {142, 52, 3},
+                {0, 21, 1, 123},
+                {1, 66, 3425, 5, 2},
+                {3, 865, 2, 123, 5}
+        };
+        for (int[] arr : mas) {
+            System.out.print(Arrays.toString(arr) + " => ");
+            sort(arr);
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    private static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] < array[j + 1]) {
+                    int tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
 }
