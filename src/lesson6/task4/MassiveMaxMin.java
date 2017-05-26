0224be0ef493e132835e81865e0508cbd2f315d5
Nikita080196 <vocabulary080196@gmail.com>
2017-05-26 21:43:38 +0300

    remake lesson6.task4

--- src/lesson6/task4/MassiveMaxMin.java
+++ src/lesson6/task4/MassiveMaxMin.java
@@ -3,38 +3,45 @@ package lesson6.task4;
 import java.util.Arrays;
 
 public class MassiveMaxMin {
+    private int[][] array = {
+            {1, 2, 3, 4, 5, 6, 7, 8},
+            {9, 10, 11, 12, 13, 14, 15, 16},
+            {17, 18, 19, 20, 21, 22, 23, 24},
+            {25, 26, 27, 28, 29, 30, 31, 32},
+            {33, 34, 35, 36, 37, 38, 39, 40}};
+    private int[][] arrayMinMax = new int[5][2];
 
-    public static final int a = 5;
-    public static final int b = 8;
-
-    public static void main(String[] args) {
-        int firstArr[][] = {
-                {1, 2, 3, 4, 5, 6, 7, 8},
-                {9, 10, 11, 12, 13, 14, 15, 16},
-                {17, 18, 19, 20, 21, 22, 23, 24},
-                {25, 26, 27, 28, 29, 30, 31, 32},
-                {33, 34, 35, 36, 37, 38, 39, 40}
-        };
-        System.out.println(Arrays.deepToString(firstArr));
-        minMax(firstArr);
-    }
-
-    public static void minMax(int[][] start) {
-        int secondArr[][] = new int[a][2];
-        for (int i = 0; i < b; i++) {
-            int min = start[i][0];
-            int max = start[i][0];
-            for (int j = 0; j < b; j++) {
-                if (start[i][j] < min) {
-                    min = start[i][j];
+    public void ArrayMinMax() {
+        for (int i = 0; i < array.length; i++) {
+            int min = 0;
+            int max = 0;
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] < min) {
+                    min = array[i][j];
                 }
-                if (start[i][j] > max) {
-                    max = start[i][j];
+                if (array[i][j] > max) {
+                    max = array[i][j];
                 }
             }
-            secondArr[i][0] = min;
-            secondArr[i][1] = max;
+            arrayMinMax[i][0] = min;
+            arrayMinMax[i][1] = max;
         }
-        System.out.println(Arrays.deepToString(secondArr));
+    }
+
+    public void printArr() {
+        String a = Arrays.deepToString(array);
+        System.out.println(a);
+    }
+
+    public void printMassiveMinMax() {
+        String b = Arrays.deepToString(arrayMinMax);
+        System.out.println(b);
+    }
+
+    public static void main(String[] args) {
+        MassiveMaxMin massiveMaxMin = new MassiveMaxMin();
+        massiveMaxMin.ArrayMinMax();
+        massiveMaxMin.printArr();
+        massiveMaxMin.printMassiveMinMax();
     }
 }
--- src/lesson8/task5/Main.java
+++ src/lesson8/task5/Main.java
@@ -4,4 +4,5 @@ package lesson8.task5;
  * Created by Валентина on 16.05.2017.
  */
 public class Main {
+
 }
