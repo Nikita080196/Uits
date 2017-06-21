7c20022801af7b638976605acd3871a0b858bfaa
Nikita080196 <vocabulary080196@gmail.com>
2017-06-21 20:53:28 +0300

    Inner classes

---
+++ src/lesson18/User.java
@@ -0,0 +1,43 @@
+package lesson18;
+
+import java.util.Queue;
+
+public class User {
+    private String login;
+    private String password;
+
+    public void createQuery() {
+        Query query = new Query();
+        query.printToLog();
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public class Query {
+        public void printToLog() {
+            System.out.println("Пользователь c логином " + getLogin() + " и паролем " + getPassword() + " существует");
+        }
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.createQuery();
+        Query query = user.new Query();
+
+    }
+}
