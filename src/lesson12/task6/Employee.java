5a4f0e359cfca66786a38b94a5d56615d1d78da8
Nikita080196 <vocabulary080196@gmail.com>
2017-08-22 11:47:10 +0300

    fix lesson12.task6 Report

--- src/lesson12/task6/Employee.java
+++ src/lesson12/task6/Employee.java
@@ -1,35 +1,61 @@
 package lesson12.task6;
 
 public class Employee {
-        private String fullName;
-        private double salary;
+    private String fullName;
+    private double salary;
 
-        public Employee(String fullName, double salary) {
-            this.fullName = fullName;
-            this.salary = salary;
-        }
-
-        public String getFullName() {
-            return fullName;
-        }
-
-        public void setFullName(String fullName) {
-            this.fullName = fullName;
-        }
-
-        public double getSalary() {
-            return salary;
-        }
-
-        public void setSalary(double salary) {
-            this.salary = salary;
-        }
-
-        public static void main(String[] args) {
-            Employee[] employee = new Employee[3];
-            employee[0] = new Employee("Никита", 13033.5252);
-            employee[1] = new Employee("Вася", 50045.22425);
-            employee[2] = new Employee("Петя", 10420.521);
-            EmployeeReport.generateReport(employee);
-        }
+    public Employee(String fullName, double salary) {
+        this.fullName = fullName;
+        this.salary = salary;
     }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public static void main(String[] args) {
+        Employee[] employee = new Employee[3];
+        employee[0] = new Employee("Никита", 13033.5252);
+        employee[1] = new Employee("Вася", 50045.22425);
+        employee[2] = new Employee("Петя", 10420.521);
+        EmployeeReport.generateReport(employee);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (Double.compare(employee.salary, salary) != 0) return false;
+        return fullName != null ? fullName.equals(employee.fullName) : employee.fullName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        result = fullName != null ? fullName.hashCode() : 0;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
