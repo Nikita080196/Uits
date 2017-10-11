94d33c36bb81752dbe6fdc9b39a6a358fa07f8c1
Nikita080196 <vocabulary080196@gmail.com>
2017-10-11 13:06:33 +0300

    make lesson15.task4 class Student

--- src/lesson15/task5/Demo.java
+++ src/lesson15/task5/Demo.java
@@ -9,8 +9,10 @@ public class Demo {
     public static void main(String[] args) {
 
         CompareName comporatorName = new CompareName();
+        ComparePrice comparatorPrice = new ComparePrice();
         Set<Goods> products2 = new TreeSet<>(comporatorName);
         Set<Goods> products1 = new TreeSet<>();
+        Set<Goods> products3 = new TreeSet<>(comparatorPrice);
         Goods goods1 = new Goods("Цепочка", 102, 15);
         Goods goods2 = new Goods("Джинсы", 400, 10);
         Goods goods3 = new Goods("Носки", 10, 4);
@@ -22,6 +24,10 @@ public class Demo {
         products2.add(goods2);
         products2.add(goods3);
 
+        products3.add(goods1);
+        products3.add(goods2);
+        products3.add(goods3);
+
         Category category1 = new Category("Мужская ", products1);
         Category category2 = new Category("Женская", products2);
 
@@ -29,5 +35,7 @@ public class Demo {
         printProduct(products1);
         System.out.println("Для женщин");
         printProduct(products2);
+        System.out.println("По цене:");
+        printProduct(products3);
     }
 }
